066c2ac2901a19ac4da22da86cda1d60de3d4f0c
lalit yenni <lalit.yenni@gmail.com>
2025-12-02 12:02:27 -0700

    Initial ASP.NET Core Web App

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SampleWebapp.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
